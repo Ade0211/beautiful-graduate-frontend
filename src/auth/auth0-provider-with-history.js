d3c866835a90fcf035bb47684be0e4cd1346e0ac
Ade <62720742+Ade0211@users.noreply.github.com>
2021-01-23 15:50:23 +0000

    first commit

---
+++ src/auth/auth0-provider-with-history.js
@@ -0,0 +1,29 @@
+// src/auth/auth0-provider-with-history.js
+
+import React from "react";
+import { useHistory } from "react-router-dom";
+import { Auth0Provider } from "@auth0/auth0-react";
+
+const Auth0ProviderWithHistory = ({ children }) => {
+  const domain = "dev-7y3aupqx.us.auth0.com";
+  const clientId = "0bX0vBcBZK52VRyx7XFcj6smus9KgVuU";
+
+  const history = useHistory();
+
+  const onRedirectCallback = (appState) => {
+    history.push(appState?.returnTo || window.location.pathname);
+  };
+
+  return (
+    <Auth0Provider
+      domain={domain}
+      clientId={clientId}
+      redirectUri={window.location.origin}
+      onRedirectCallback={onRedirectCallback}
+    >
+      {children}
+    </Auth0Provider>
+  );
+};
+
+export default Auth0ProviderWithHistory;
